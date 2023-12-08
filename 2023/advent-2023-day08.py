6c66e49b584d541f609a7fb0dcb084870bd16222
Zeth Weissman <4612688+zweissman@users.noreply.github.com>
2023-12-08 01:20:30 -0500

    chore: advent 2023 day 8

--- 2023/advent-2023-day08.py
+++ 2023/advent-2023-day08.py
@@ -1,10 +1,12 @@
 # pylint: disable=too-many-return-statements,too-many-statements,too-many-branches,duplicate-code,unused-argument
-# pylint: disable=unnecessary-list-index-lookup, E701
-
-FILE_NAME = "2023/input/08.txt"
+# pylint: disable=unnecessary-list-index-lookup
 
 import itertools
 import math
+from collections.abc import Iterator
+
+FILE_NAME = "2023/input/08.txt"
+
 
 def run(part: int, test_run: bool = False, debug: bool = False):
     if test_run:
@@ -30,29 +32,30 @@ def part1(data: list[str], debug: bool = False) -> int:
 
     for line in data:
         node, options = line.split(" = ")
-        options = options.replace("(", "").replace(")", "").split(", ")
-        nodes[node] = (options[0], options[1])
-
+        left, right = options.replace("(", "").replace(")", "").split(", ")
+        nodes[node] = (left, right)
 
     path = "AAA"
 
     get_direction = get_next_direction(directions)
     while path != "ZZZ":
         direction = next(get_direction)
-        if debug: print(f"Direction: {direction}")
+        if debug:
+            print(f"Direction: {direction}")
         if direction == "L":
             path = nodes[path][0]
         else:
             path = nodes[path][1]
-        if debug: print (f"Path: {path}")
+        if debug:
+            print(f"Path: {path}")
         results += 1
-
 
     return results
 
-def get_next_direction(directions):
-    for c in itertools.cycle(directions):
-        yield c
+
+def get_next_direction(directions: list[str]) -> Iterator[str]:
+    yield from itertools.cycle(directions)
+
 
 def part2(data: list[str], debug: bool = False) -> int:
     directions = list(data.pop(0))
@@ -63,43 +66,44 @@ def part2(data: list[str], debug: bool = False) -> int:
 
     for line in data:
         node, options = line.split(" = ")
-        options = options.replace("(", "").replace(")", "").split(", ")
-        nodes[node] = (options[0], options[1])
+        left, right = options.replace("(", "").replace(")", "").split(", ")
+        nodes[node] = (left, right)
 
-    if debug: print(nodes.keys())
+    if debug:
+        print(nodes.keys())
 
-    for start_path in [node for node in nodes.keys() if node.endswith("A")]:
-        if debug: print (f"Start Path: {start_path}")
+    for start_path in [node for node, _ in nodes.items() if node.endswith("A")]:
+        if debug:
+            print(f"Start Path: {start_path}")
         results = 0
 
-    
         get_direction = get_next_direction(directions)
 
         path = start_path
 
         while not path.endswith("Z"):
             direction = next(get_direction)
-            if debug: print(f"Direction: {direction}")
+            if debug:
+                print(f"Direction: {direction}")
             if direction == "L":
                 path = nodes[path][0]
             else:
                 path = nodes[path][1]
-            if debug: print (f"Path: {path}")
+            if debug:
+                print(f"Path: {path}")
             results += 1
 
         result_list.append(results)
 
-
-    if debug: print(result_list)
+    if debug:
+        print(result_list)
 
     return math.lcm(*result_list)
 
 
-
-
 if __name__ == "__main__":
-    #final = run(part=1, test_run=True, debug=True)  # 6
-    #final = run(part=1, test_run=False, debug=False)  # 12361
-    #final = run(part=2, test_run=True, debug=True) # 6
+    # final = run(part=1, test_run=True, debug=True)  # 6
+    # final = run(part=1, test_run=False, debug=False)  # 12361
+    # final = run(part=2, test_run=True, debug=True) # 6
     final = run(part=2, test_run=False, debug=False)  # 18215611419223
     print("ANSWER:", final)
