2cadb57d7c76d218e49d2c8ea341cc326dd700a9
Zeth Weissman <4612688+zweissman@users.noreply.github.com>
2023-12-12 15:22:30 -0500

    Update advent-2023-day12.py

--- 2023/advent-2023-day12.py
+++ 2023/advent-2023-day12.py
@@ -3,8 +3,9 @@
 
 FILE_NAME = "2023/input/12.txt"
 
-from math import comb
-from functools  import reduce
+
+from itertools import product
+
 
 def run(part: int, test_run: bool = False, debug: bool = False):
     if test_run:
@@ -21,175 +22,55 @@ def run(part: int, test_run: bool = False, debug: bool = False):
     return part_function(data=data, debug=debug)
 
 
-def show(grid: list[str], debug: bool = False):
-    if debug:
-        print()
-        for y in range(len(grid)):
-            print("".join([str(x) for x in grid[y]]))
-
-def is_invalid(elements, values)->bool:
-    if len(elements) > len(values):
-        return True
-
-    for i in range(len(elements)):
-        element = elements[i]
-        value = values[i]
-        if len(element) < value:
-            return True
-    
-    print(elements, values)
-    return False
 
 def part1(data: list[str], debug: bool = False) -> int:
+
     results = 0
-
-    max_grid = 0
-    grid=[]
-    for d in data:
-        if not d.startswith("//"):
-            grid.append(d.strip())
-
-    #show(grid,debug)
-
-    while len(grid) > 0:
-        # max_grid = max(max_grid, len(grid))
-        if len(grid) % 10000 == 0:
-            print(len(grid))
-        row = grid.pop(0)
-        spring_str, value_str = row.split(" ")
-        if debug: print (spring_str)
-
-        spring_str,value_str= reduce_elements(spring_str, value_str, debug)
-
-        if spring_str == "":
+    for line in data:
+        if line.startswith("//"):
             continue
 
-        first_unknown = spring_str.find("?")
-        if first_unknown > -1:
-            grid.append(spring_str.replace("?", "#", 1) + " " + value_str)
-            grid.append(spring_str.replace("?", ".", 1) + " " + value_str)
+        springs, values = line.split(" ")
+        springs = springs.replace(".", " ")
 
+        result = get_variation_count(springs, values)
+        if debug: print (springs, values, result)
+        results += result
 
-        else:
-            elements = spring_str.split(".")
-            elements = [x for x in elements if x != ""]
-
-            values = value_str.split(",")
-            values = [int(x) for x in values]
-            
-
-            if len(elements) != len(values):
-                continue
-
-            for i in range(len(elements)):
-                element = elements[i]
-                value = values[i]
-
-                if len(element) != value:
-                    break
-
-            else:
-                results += 1
-                if debug: print (elements, values)
-                if debug: print("Found one")
-
-    print(f"{max_grid=}")
     return results
 
+def get_variation_count(springs: str, values: str) -> int:
+    variations = []
+    for variation in product(" #", repeat=springs.count("?")):
+        variations.append("".join(variation))
+   
+    results = []
+    for variation in variations:
+        result = get_valid_count(merge_run(springs, variation), values)
+        results.append(result)
 
-def reduce_elements(element_str:str, value_str:str, debug:bool) -> tuple[list[str], list[int]]:
-
-    while element_str.find('..') > -1:
-        element_str = element_str.replace('..', '.').strip('.')
-
-    # elements = element_str.split(".")
-    # elements = [x for x in elements if x != ""]
-    # values = value_str.split(",")
-    # values = [int(x) for x in values]
-
-    words = element_str.strip('.').split(".")
-    values = value_str.split(",")
-
-    i = 0
-    word = words[i]
-    value = int(values[i])
-
-    if len(word) < value:
-        # Word is too short
-        return "", value_str
-
-    first_not_spring = word.find("?")
-    if first_not_spring == -1 and len(word) != value:
-        # Word is too long
-        return "", value_str
-
-    if len(word) == value:
-        words[i] = "#" * value
-
-    elif word.startswith('?') and word.endswith('?'):
-        if len(word) - 1 == value:
-            updated_word = "?" + "#" * (value - 1) + "?"
-            words[i] = updated_word
+    return sum(results)
 
 
-    i = -1
-    word = words[i]
-    value = int(values[i])
+def get_valid_count(springs: str, values: str) -> int:
+    spring_groups = springs.split()
+    run_lengths = []
+    for springs in spring_groups:
+        run_lengths.append(str(len(springs)))
 
-    if len(word) < value:
-        # Word is too short
-        return "", value_str
-
-    first_not_spring = word.find("?")
-    if first_not_spring == -1 and len(word) != value:
-        # Word is too long
-        return "", value_str
-
-    if len(word) == value:
-        words[i] = "#" * value
-
-    elif word.startswith('?') and word.endswith('?'):
-        if len(word) - 1 == value:
-            updated_word = "?" + "#" * (value - 1) + "?"
-            words[i] = updated_word
-
-    return ".".join(words), value_str
-
-    # # Find the first element with a ? in it
-    # for i in range(len(elements)):
-    #     element = elements[i]
-    #     value = values[i]
-
-    #     for x in range(value, 0 , -1):
-    #         first_index = element.find("#" * x)
-    #         if -1 < first_index <= value:
-    #             test_string = element[max(0, first_index-value):first_index+value]
-    #             if test_string.count("#") == value:
-    #                 # We found what what we are looking for.
-    #                 element = element[first_index+value+1:] # Take an extra off for the spacer
-    #                 elements[i] = element
-    #                 values.pop(i)
-
-    #                 return reduce_elements(elements, values, debug)
-    #             else:
-    #                 print("do work")
-    #                 pass
-    #     else:
-    #         # All we have a ? in this element
-    #         return elements, values
-
-    #     if debug: print (elements, values)
-
-    # return elements, values
-
-def get_element_count(element: str) -> int:
-    while element.find('..') > -1:
-        element = element.replace('..', '.').strip('.')
-
-    return element.count(".") + 1
+    return ",".join(run_lengths) == values
 
 
-def part2(data: list[str], empty_row_count: int = 999999, debug: bool = False) -> int:
+def merge_run(springs: str, subs: str) -> str:
+    assert springs.count("?") == len(subs)
+    
+    for c in subs:
+        springs = springs.replace("?", c, 1)
+        
+    return springs
+
+
+def part2(data: list[str], debug: bool = False) -> int:
     results = 0
 
     grid = []
@@ -198,14 +79,18 @@ def part2(data: list[str], empty_row_count: int = 999999, debug: bool = False) -
         d = d.strip()
         grid.append(list(d))
 
-    show(grid, debug)
 
 
     return results
 
 
+import time
+
 if __name__ == "__main__":
+    start_time = time.time()
     print("Test1: ", run(part=1, test_run=True, debug=True))  # 21
-    #print("Real1: ", run(part=1, test_run=False, debug=False))  # 5686 too low
+    #print("Real1: ", run(part=1, test_run=False, debug=False))  # 6949
     # print("Test2: ", run(part=2, test_run=True, debug=True)) # 
     #print("Real2: ", run(part=2, test_run=False, debug=False))  # 
+
+    print(time.time() - start_time)
