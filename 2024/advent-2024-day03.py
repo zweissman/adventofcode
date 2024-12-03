a336b718898b8bc0902db363e74eb64a7e17f15e
Zeth Weissman <4612688+zweissman@users.noreply.github.com>
2024-12-03 15:07:07 -0500

    chore: a way better solution for day 3 part 2

--- 2024/advent-2024-day03.py
+++ 2024/advent-2024-day03.py
@@ -39,50 +39,21 @@ def part1(data: list[str], debug: bool = False) -> int:
 
 
 def part2(data: list[str], debug: bool = False) -> int:
-    results = 0
     # Start with a do() operand
     text = "do()" + "".join(data)
 
     if debug:
         print(text)
 
-    pattern = r"(don't|do)\(\)(.*?)mul\((\d{1,3}),(\d{1,3})\)"
-    regex = re.compile(pattern)
+    # Remove all the don't() ... do(), then rerun part 1
+    pattern = r"don't\(\).*?(?:do\(\)|$)"
+    text = re.sub(pattern, "", text)
 
-    while True:
-        first_match = re.search(pattern, text)
-
-        if debug:
-            print(first_match)
-
-        if not first_match:
-            break
-
-        matches = regex.findall(text[first_match.span()[0] : first_match.span()[1]])
-
-        if len(matches) > 0:
-            op, inside, a, b = matches[0]
-            if "do()" in inside or "don't()" in inside:
-                # Take off the current leading operation, and let the next one dictate what to do
-                text = text[len(op) + 2 :]
-                continue
-
-            if op == "do":
-                if debug:
-                    print("do", a, b)
-                results += int(a) * int(b)
-            else:
-                if debug:
-                    print("don't", a, b)
-
-        # run again with the current operation
-        text = op + "()" + text[first_match.span()[1] :]
-
-    return results
+    return part1([text], debug)
 
 
 if __name__ == "__main__":
     # print("Test1: ", run(part=1, test_run=True, debug=True))  # 161
-    # print("Real1: ", run(part=1, test_run=False, debug=False))  #   168539636
+    # print("Real1: ", run(part=1, test_run=False, debug=False))  # 168539636
     # print("Test2: ", run(part=2, test_run=True, debug=True))  # 48
     print("Real2: ", run(part=2, test_run=False, debug=False))  # 97529391
