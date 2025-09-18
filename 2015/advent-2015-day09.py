c8a8802915358c3b54e2d8a108cd2ec7e61f3057
Zeth Weissman <4612688+zweissman@users.noreply.github.com>
2025-09-18 14:02:04 -0400

    chore: adding 2015 Day 9

---
+++ 2015/advent-2015-day09.py
@@ -0,0 +1,110 @@
+import itertools
+
+
+def run(part: int, test_suffix: str = "", debug: bool = False):
+    y, d = __file__.split("advent-")[1].split("-day")
+    file_name = f"{y}/input/{d.strip('.py')}{test_suffix}.txt"
+
+    with open(file_name, encoding="utf-8") as file:
+        file_data = file.readlines()
+
+    data = [x.strip() for x in file_data if not x.startswith("#")]
+    part_function = part1 if part == 1 else part2
+
+    return part_function(data=data, debug=debug)
+
+
+def part1(data: list[str], debug: bool = False) -> int:
+    results = 0
+    show(data, debug=debug)
+    lookup: dict[tuple[str, str], int] = {}
+    cities: set[str] = set()
+
+    for row in data:
+        cities_string, distance = row.split(" = ")
+        city1, city2 = cities_string.split(" to ")
+        lookup[(city1, city2)] = int(distance)
+        lookup[(city2, city1)] = int(distance)
+
+        cities.update([city1, city2])
+
+    results = find_shortest_route(lookup, cities, debug)
+
+    return results
+
+
+def find_shortest_route(
+    lookup: dict[tuple[str, str], int], cities: set[str], debug: bool = False
+) -> int:
+    shortest_distance = 99999999
+
+    for route in itertools.permutations(cities):
+        distance = 0
+        start = route[0]
+        path = start
+        for city in route[1:]:
+            distance += lookup.get((start, city), 0)
+            start = city
+            path += ", " + city
+
+        if distance < shortest_distance:
+            shortest_distance = distance
+            if debug:
+                print("New shortest route:" + path)
+
+    return shortest_distance
+
+
+def part2(data: list[str], debug: bool = False) -> int:
+    results = 0
+    show(data, debug=debug)
+    lookup: dict[tuple[str, str], int] = {}
+    cities: set[str] = set()
+
+    for row in data:
+        cities_string, distance = row.split(" = ")
+        city1, city2 = cities_string.split(" to ")
+        lookup[(city1, city2)] = int(distance)
+        lookup[(city2, city1)] = int(distance)
+
+        cities.update([city1, city2])
+
+    results = find_longest_route(lookup, cities, debug)
+
+    return results
+
+
+def find_longest_route(
+    lookup: dict[tuple[str, str], int], cities: set[str], debug: bool = False
+) -> int:
+    longest_distance = 0
+
+    for route in itertools.permutations(cities):
+        distance = 0
+        start = route[0]
+        path = start
+        for city in route[1:]:
+            distance += lookup.get((start, city), 0)
+            start = city
+            path += ", " + city
+
+        if distance > longest_distance:
+            longest_distance = distance
+            if debug:
+                print("New longest route:" + path)
+
+    return longest_distance
+
+
+def show(grid: list[str], debug: bool = False) -> None:
+    if debug:
+        print()
+        for y, row in enumerate(grid):
+            print(f"{y:02}", "".join([str(x) for x in row]))
+
+
+if __name__ == "__main__":
+    # print("Test1: ", run(part=1, test_suffix="-test", debug=True))  # 605
+    # print("Real1: ", run(part=1, debug=False))  # 141
+    # print("Test2: ", run(part=2, test_suffix="-test", debug=True))  # 982
+    print("Real2: ", run(part=2, debug=False))  # 736
