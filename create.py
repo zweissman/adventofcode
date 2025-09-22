0d71141dbc8d2d39c2d74171313792f881348bcc
Zeth Weissman <4612688+zweissman@users.noreply.github.com>
2025-09-22 12:06:33 -0400

    chore: adding checks to make sure we don't overwrite existing files

--- create.py
+++ create.py
@@ -9,8 +9,13 @@ parser.add_argument("day", type=int, help="The day of the puzzle", nargs=1)
 parser.add_argument("year", type=int, help="The year of the puzzle", nargs=1)
 args = parser.parse_args()
 
-day = f"{args.day[0]:02d}"
+day = args.day[0]
 year = args.year[0]
+
+if day > year:
+    year, day = day, year
+
+day = f"{day:02d}"
 
 if not Path.exists(Path(f"{year}")):
     Path.mkdir(Path(f"{year}"))
@@ -18,7 +23,16 @@ if not Path.exists(Path(f"{year}")):
 if not Path.exists(Path(f"{year}/input")):
     Path.mkdir(Path(f"{year}/input"))
 
-copyfile(Path(TEMPLATE), f"{year}/advent-{year}-day{day}.py")
-Path(f"{year}/input/{day}.txt").touch()
-Path(f"{year}/input/{day}-test.txt").touch()
+code_file = f"{year}/advent-{year}-day{day}.py"
+if not Path.exists(Path(code_file)):
+    copyfile(Path(TEMPLATE), code_file)
+
+input_file = f"{year}/input/{day}.txt"
+if not Path.exists(Path(input_file)):
+    Path(f"{year}/input/{day}.txt").touch()
+
+input_test_file = f"{year}/input/{day}-test.txt"
+if not Path.exists(Path(input_test_file)):
+    Path(input_test_file).touch()
+
 print(f"Created files for Advent of Code {year} Day {day}")
